446c2f227acbf6647aa3a45cad11f4d00a82aa08
Matthew Griffith <mdg.griffith@gmail.com>
2020-01-04 21:16:53 -0500

    add tests to ensure oscillations are firing.

--- tests/Oscillations.elm
+++ tests/Oscillations.elm
@@ -12,6 +12,49 @@ import Test exposing (..)
 import Time
 
 
+type House
+    = Hufflepuff
+    | Griffyndor
+    | Slytherin
+    | Ravenclaw
+
+
+toPosition event =
+    case event of
+        Hufflepuff ->
+            Animator.wave 0 1
+                |> Animator.loop (Animator.millis 100)
+
+        Griffyndor ->
+            Animator.to 300
+
+        Slytherin ->
+            Animator.to 700
+
+        Ravenclaw ->
+            Animator.to 1000
+
+
+single =
+    Animator.init Hufflepuff
+        |> Animator.update (Time.millisToPosix 0)
+        |> Animator.update (Time.millisToPosix 50)
+
+
+four =
+    Animator.init Hufflepuff
+        |> Animator.queue
+            [ Animator.wait (Animator.seconds 0.5)
+            , Animator.event (Animator.seconds 1) Griffyndor
+            , Animator.wait (Animator.seconds 1.0)
+            , Animator.event (Animator.seconds 1) Slytherin
+            , Animator.wait (Animator.seconds 1.0)
+            , Animator.event (Animator.seconds 1) Ravenclaw
+            , Animator.wait (Animator.seconds 1.0)
+            ]
+        |> Animator.update (Time.millisToPosix 0)
+
+
 oscillations =
     describe "oscillations"
         [ test "wave" <|
@@ -41,4 +84,24 @@ oscillations =
 
                     _ ->
                         Expect.fail "Creating an oscillator did not create an oscillator"
+        , test "wave on timeline, halfwy" <|
+            \_ ->
+                let
+                    { position, velocity } =
+                        Animator.move single toPosition
+                in
+                Expect.within
+                    (Absolute 0.001)
+                    position
+                    1.0
+        , test "wave on timeline, second period" <|
+            \_ ->
+                let
+                    { position, velocity } =
+                        Animator.move (Animator.update (Time.millisToPosix 150) single) toPosition
+                in
+                Expect.within
+                    (Absolute 0.001)
+                    position
+                    1.0
         ]
