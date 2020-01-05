86c7d9e00f163dff5e6c6969948a2b86c56f453a
Matthew Griffith <mdg.griffith@gmail.com>
2020-01-05 12:27:35 -0500

    More queueing and interruption tests

--- tests/Scheduling.elm
+++ tests/Scheduling.elm
@@ -18,6 +18,7 @@ type Event
     | Two
     | Three
     | Four
+    | Five
     | Unreachable
 
 
@@ -51,6 +52,9 @@ toVals event =
         Four ->
             4
 
+        Five ->
+            5
+
         Unreachable ->
             -1
 
@@ -82,36 +86,119 @@ queueing =
                 |> Animator.update (Time.millisToPosix 0)
     in
     describe "Queueing"
-        [ test "Simple queue" <|
+        [ describe "Simple"
+            [ test "Simple queue" <|
+                \_ ->
+                    Expect.equal
+                        newTimeline
+                        (Timeline.Timeline
+                            { events =
+                                Timeline.Timetable
+                                    [ Timeline.Line (qty 0)
+                                        (occur Starting (qty 0) (Just (qty 1)))
+                                        [ occur One (qty 2000) (Just (qty 1))
+                                        , occur Two (qty 4000) (Just (qty 1))
+                                        , occur Three (qty 6000) (Just (qty 1))
+                                        ]
+                                    ]
+                            , initial = Starting
+                            , interruption = []
+                            , now = qty 0
+                            , queued = Nothing
+                            , running = True
+                            }
+                        )
+            , test "Resulting timeline foldp's correctly" <|
+                \_ ->
+                    Expect.all
+                        [ valueAtEquals 0 0
+                        , valueAtEquals 2000 1
+                        , valueAtEquals 4000 2
+                        , valueAtEquals 6000 3
+                        ]
+                        newTimeline
+            ]
+        , test "Queue after some time" <|
             \_ ->
+                let
+                    queued =
+                        timeline
+                            |> Animator.update (Time.millisToPosix 3000)
+                            |> Animator.queue
+                                [ Animator.event (Animator.seconds 1) One
+                                ]
+                            |> Animator.update (Time.millisToPosix 4000)
+                in
+                -- specifically we expect the dwell time on `Starting` to be extended
                 Expect.equal
-                    newTimeline
+                    queued
                     (Timeline.Timeline
                         { events =
                             Timeline.Timetable
                                 [ Timeline.Line (qty 0)
-                                    (occur Starting (qty 0) (Just (qty 1)))
-                                    [ occur One (qty 2000) (Just (qty 1))
-                                    , occur Two (qty 4000) (Just (qty 1))
-                                    , occur Three (qty 6000) (Just (qty 1))
+                                    (occur Starting (qty 0) (Just (qty 4)))
+                                    [ occur One (qty 5000) Nothing
                                     ]
                                 ]
                         , initial = Starting
                         , interruption = []
-                        , now = qty 0
+                        , now = qty 4000
                         , queued = Nothing
                         , running = True
                         }
                     )
-        , test "Resulting timeline foldp's correctly" <|
+        , test "Queue twice after some time" <|
             \_ ->
-                Expect.all
-                    [ valueAtEquals 0 0
-                    , valueAtEquals 2000 1
-                    , valueAtEquals 4000 2
-                    , valueAtEquals 6000 3
-                    ]
-                    newTimeline
+                let
+                    queued =
+                        timeline
+                            |> Animator.update (Time.millisToPosix 3000)
+                            |> Animator.queue
+                                [ Animator.event (Animator.seconds 1) One
+                                ]
+                            |> Animator.update (Time.millisToPosix 4000)
+                            |> Animator.queue
+                                [ Animator.event (Animator.seconds 1) Two
+                                ]
+                            |> Animator.update (Time.millisToPosix 7000)
+                in
+                -- specifically we expect the dwell time on `Starting` and `One` to be extended
+                Expect.equal
+                    queued
+                    (Timeline.Timeline
+                        { events =
+                            Timeline.Timetable
+                                [ Timeline.Line (qty 0)
+                                    (occur Starting (qty 0) (Just (qty 4)))
+                                    [ occur One (qty 5000) (Just (qty 2))
+                                    , occur Two (qty 8000) Nothing
+                                    ]
+                                ]
+                        , initial = Starting
+                        , interruption = []
+                        , now = qty 7000
+                        , queued = Nothing
+                        , running = True
+                        }
+                    )
+        , test "Adding an initial wait is equivalent to simply updating later" <|
+            \_ ->
+                Expect.equal
+                    (timeline
+                        |> Animator.queue
+                            [ Animator.wait (Animator.seconds 2)
+                            , Animator.event (Animator.seconds 1) One
+                            ]
+                        |> Animator.update (Time.millisToPosix 0)
+                        -- move the clock forward to 2s so that `now` matches
+                        |> Animator.update (Time.millisToPosix 2000)
+                    )
+                    (timeline
+                        |> Animator.queue
+                            [ Animator.event (Animator.seconds 1) One
+                            ]
+                        |> Animator.update (Time.millisToPosix 2000)
+                    )
         ]
 
 
@@ -124,8 +211,6 @@ interruptions =
                     , Animator.event (Animator.seconds 1) One
                     , Animator.wait (Animator.seconds 1.0)
                     , Animator.event (Animator.seconds 1) Two
-
-                    --
                     , Animator.wait (Animator.seconds 1.0)
                     , Animator.event (Animator.seconds 1) Unreachable
                     , Animator.wait (Animator.seconds 1.0)
@@ -231,6 +316,96 @@ interruptions =
                         , initial = Starting
                         , interruption = []
                         , now = qty 4500
+                        , queued = Nothing
+                        , running = True
+                        }
+                    )
+        , test "Queue timeline instead of interrupt if given after the existing timeline" <|
+            \_ ->
+                let
+                    baseline =
+                        timeline
+                            |> Animator.queue
+                                [ Animator.wait (Animator.seconds 1.0)
+                                , Animator.event (Animator.seconds 1) One
+                                , Animator.wait (Animator.seconds 1.0)
+                                , Animator.event (Animator.seconds 1) Two
+                                , Animator.wait (Animator.seconds 1.0)
+                                , Animator.event (Animator.seconds 1) Three
+                                ]
+                            |> Animator.update (Time.millisToPosix 0)
+
+                    interruptedAfterFinish =
+                        baseline
+                            |> Animator.interrupt
+                                [ Animator.event (Animator.seconds 1) Four
+                                , Animator.wait (Animator.seconds 1.0)
+                                , Animator.event (Animator.seconds 1) Five
+                                ]
+                            |> Animator.update (Time.millisToPosix 6000)
+                in
+                Expect.equal
+                    interruptedAfterFinish
+                    (Timeline.Timeline
+                        { events =
+                            Timeline.Timetable
+                                [ Timeline.Line (qty 0)
+                                    (occur Starting (qty 0) (Just (qty 1)))
+                                    [ occur One (qty 2000) (Just (qty 1))
+                                    , occur Two (qty 4000) (Just (qty 1))
+                                    , occur Three (qty 6000) (Just (qty 0))
+                                    , occur Four (qty 7000) (Just (qty 1))
+                                    , occur Five (qty 9000) Nothing
+                                    ]
+                                ]
+                        , initial = Starting
+                        , interruption = []
+                        , now = qty 6000
+                        , queued = Nothing
+                        , running = True
+                        }
+                    )
+        , test "Queue timeline instead if given long after existing" <|
+            \_ ->
+                let
+                    baseline =
+                        timeline
+                            |> Animator.queue
+                                [ Animator.wait (Animator.seconds 1.0)
+                                , Animator.event (Animator.seconds 1) One
+                                , Animator.wait (Animator.seconds 1.0)
+                                , Animator.event (Animator.seconds 1) Two
+                                , Animator.wait (Animator.seconds 1.0)
+                                , Animator.event (Animator.seconds 1) Three
+                                ]
+                            |> Animator.update (Time.millisToPosix 0)
+
+                    interruptedAfterFinish =
+                        baseline
+                            |> Animator.interrupt
+                                [ Animator.event (Animator.seconds 1) Four
+                                , Animator.wait (Animator.seconds 1.0)
+                                , Animator.event (Animator.seconds 1) Five
+                                ]
+                            |> Animator.update (Time.millisToPosix 8000)
+                in
+                Expect.equal
+                    interruptedAfterFinish
+                    (Timeline.Timeline
+                        { events =
+                            Timeline.Timetable
+                                [ Timeline.Line (qty 0)
+                                    (occur Starting (qty 0) (Just (qty 1)))
+                                    [ occur One (qty 2000) (Just (qty 1))
+                                    , occur Two (qty 4000) (Just (qty 1))
+                                    , occur Three (qty 6000) (Just (qty 2))
+                                    , occur Four (qty 9000) (Just (qty 1))
+                                    , occur Five (qty 11000) Nothing
+                                    ]
+                                ]
+                        , initial = Starting
+                        , interruption = []
+                        , now = qty 8000
                         , queued = Nothing
                         , running = True
                         }
