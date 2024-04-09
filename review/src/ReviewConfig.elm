d952045827b4cfaa7edb742e8aa85c38ee5efa1c
Matthew Griffith <mdg.griffith@gmail.com>
2024-04-09 09:52:24 -0400

    Add elm-review (again)

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -11,29 +11,60 @@ when inside the directory containing this file.
 
 -}
 
+import Docs.NoMissing exposing (exposedModules, onlyExposed)
+import Docs.ReviewAtDocs
+import Docs.ReviewLinksAndSections
+import Docs.UpToDateReadmeLinks
+import NoConfusingPrefixOperator
 import NoDebug.Log
 import NoDebug.TodoOrToString
 import NoExposingEverything
 import NoImportingEverything
 import NoMissingTypeAnnotation
+import NoMissingTypeAnnotationInLetIn
+import NoMissingTypeExpose
+import NoPrematureLetComputation
+import NoSimpleLetBody
+import NoUnused.CustomTypeConstructorArgs
 import NoUnused.CustomTypeConstructors
 import NoUnused.Dependencies
 import NoUnused.Exports
-import NoUnused.Modules
+import NoUnused.Parameters
+import NoUnused.Patterns
 import NoUnused.Variables
-import Review.Rule exposing (Rule)
+import Review.Rule as Rule exposing (Rule)
+import Simplify
 
 
 config : List Rule
 config =
-    [ NoDebug.Log.rule
-    , NoDebug.TodoOrToString.rule
-    , NoExposingEverything.rule
-    , NoImportingEverything.rule []
-    , NoMissingTypeAnnotation.rule
-    , NoUnused.CustomTypeConstructors.rule []
-    , NoUnused.Dependencies.rule
-    , NoUnused.Exports.rule
-    , NoUnused.Modules.rule
-    , NoUnused.Variables.rule
-    ]
+    List.map (Rule.ignoreErrorsForDirectories [ "tests/" ])
+        [ -- Docs.NoMissing.rule
+          -- { document = onlyExposed
+          -- , from = exposedModules
+          -- }
+          -- ,
+          Docs.ReviewLinksAndSections.rule
+        , Docs.ReviewAtDocs.rule
+        , Docs.UpToDateReadmeLinks.rule
+        , NoConfusingPrefixOperator.rule
+        , NoDebug.Log.rule
+        , NoDebug.TodoOrToString.rule
+            |> Rule.ignoreErrorsForDirectories [ "tests/" ]
+        , NoExposingEverything.rule
+        , NoImportingEverything.rule []
+        , NoMissingTypeAnnotation.rule
+
+        -- , NoMissingTypeAnnotationInLetIn.rule
+        , NoMissingTypeExpose.rule
+        , NoSimpleLetBody.rule
+        , NoPrematureLetComputation.rule
+        , NoUnused.CustomTypeConstructors.rule []
+        , NoUnused.CustomTypeConstructorArgs.rule
+        , NoUnused.Dependencies.rule
+        , NoUnused.Exports.rule
+        , NoUnused.Parameters.rule
+        , NoUnused.Patterns.rule
+        , NoUnused.Variables.rule
+        , Simplify.rule Simplify.defaults
+        ]
